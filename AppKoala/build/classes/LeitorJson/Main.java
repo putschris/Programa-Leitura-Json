27013715ead9804caaa5e591fec6cadd836513e6
putschris <gui89cris1157@gmail.com>
2019-07-26 16:25:06 -0300

    Subindo um novo arquivo

--- AppKoala/build/classes/LeitorJson/Main.java
+++ AppKoala/build/classes/LeitorJson/Main.java
@@ -1,57 +1,62 @@
 package LeitorJson;
 
-// <editor-fold defaultstate="collapsed" desc="imports...">
-import java.io.FileNotFoundException;
+// <editor-fold defaultstate="collapsed" desc="imports...">import java.io.FileReader; 
 import java.io.FileReader;
-import java.io.IOException;
-import static java.lang.Double.parseDouble;
-import static java.lang.Float.parseFloat;
-import static java.lang.Integer.parseInt;
-import static java.lang.String.valueOf;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static javax.swing.JOptionPane.showMessageDialog;
-import static javax.swing.JOptionPane.showInputDialog;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
-import org.json.simple.*;
 
-// </editor-fold>
+public class Main {
 
-public class Main 
-{
-    
-    public static void main(String[] args) 
-    {
-//        JSONObject jsonObj;
-        JSONObject jsonObj;
-        JSONParser parser = new JSONParser();
-        DadosJson dados = new DadosJson();
-        
-        try 
+    public static void main(String[] args) throws Exception {
+        try {
+            // parsing file "JSONExample.json" 
+            Object obj = new JSONParser().parse(new FileReader("C:\\Users\\guilhermecr\\OneDrive - CATENO GESTAO DE CONTAS DE PAGAMENTO S.A\\Documentos\\NetBeansProjects\\AppKoala\\src\\JSONExemple.jsons"));
+
+            // typecasting obj to JSONObject 
+            JSONObject jo = (JSONObject) obj;
+
+            // getting firstName and lastName 
+            String firstName = (String) jo.get("firstName");
+            String lastName = (String) jo.get("lastName");
+
+            System.out.println(firstName);
+            System.out.println(lastName);
+
+            // getting age 
+            long age = (long) jo.get("age");
+            System.out.println(age);
+
+            // getting address 
+            Map address = ((Map) jo.get("address"));
+
+            // iterating address Map 
+            Iterator<Map.Entry> itr1 = address.entrySet().iterator();
+            while (itr1.hasNext()) {
+                Map.Entry pair = itr1.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+
+            // getting phoneNumbers 
+            JSONArray ja = (JSONArray) jo.get("phoneNumbers");
+
+            // iterating phoneNumbers 
+            Iterator itr2 = ja.iterator();
+
+            while (itr2.hasNext()) {
+                itr1 = ((Map) itr2.next()).entrySet().iterator();
+                while (itr1.hasNext()) {
+                    Map.Entry pair = itr1.next();
+                    System.out.println(pair.getKey() + " : " + pair.getValue());
+                }
+            }
+        } catch (Exception e) 
         {
-            
-            jsonObj = (JSONObject) parser.parse(new FileReader("C:\\Users\\guilhermecr\\OneDrive - CATENO GESTAO DE CONTAS DE PAGAMENTO S.A\\Documentos\\NetBeansProjects\\AppKoala\\src\\LeitorJson\\norris.json"));
-            
-//            System.out.println(jsonObj.get("value"));
-            JSONValue value = (JSONValue) jsonObj.get("value");
-            System.out.println(value);
-                    
-//            dados.setType((String) jsonObj.get("type"));
-//            dados.setJoke((String) jsonObj.get("joke"));
-//            dados.setId((String) jsonObj.get("id"));
-//            dados.setCategories((String) jsonObj.get("categories"));
-//            
-//            System.out.println("Ai está: " + "\n" + dados.toString());
-;
-            
-        }catch(Exception e) 
-        {
-            e.printStackTrace();
+            System.out.println("Erro no servidor!");
         }
+
     }
-    
 }
--- AppKoala/src/LeitorJson/Main.java
+++ AppKoala/src/LeitorJson/Main.java
@@ -1,58 +1,73 @@
 package LeitorJson;
 
-// <editor-fold defaultstate="collapsed" desc="imports...">
-import java.io.FileNotFoundException;
+// <editor-fold defaultstate="collapsed" desc="imports...">import java.io.FileReader; 
 import java.io.FileReader;
-import java.io.IOException;
-import static java.lang.Double.parseDouble;
-import static java.lang.Float.parseFloat;
-import static java.lang.Integer.parseInt;
-import static java.lang.String.valueOf;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static javax.swing.JOptionPane.showMessageDialog;
-import static javax.swing.JOptionPane.showInputDialog;
+import java.util.Iterator;
+import java.util.Map;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.j
-// </editor-fold>
+public class Main {
 
-public class Main 
-{
-    
-    public static void main(String[] args) 
-    {
-//        JSONObject jsonObj;
-        JsonObject jsonObj;
-        JsonParser parser = new JsonParser();
-        DadosJson dados = new DadosJson();
-        
-        try 
+    public static void main(String[] args) throws Exception {
+        try {
+            // parsing file "JSONExample.json" 
+            Object obj = new JSONParser().parse(new FileReader("C:\\Users\\guilhermecr\\OneDrive - CATENO GESTAO DE CONTAS DE PAGAMENTO S.A\\Documentos\\NetBeansProjects\\AppKoala\\src\\LeitorJson\\JSONExample.json"));
+
+            // typecasting obj to JSONObject 
+            // transformando o resultado do obj em JSONObject
+            JSONObject jo = (JSONObject) obj;
+
+            // getting firstName and lastName 
+            // Obtendo o primeiro e último nome
+            // transformando o resultado das variáveis em String
+            String firstName = (String) jo.get("firstName");
+            String lastName = (String) jo.get("lastName");
+
+            System.out.println(firstName);
+            System.out.println(lastName);
+
+            // getting age 
+            // casting - transformando o resultado do GET em long
+            long age = (long) jo.get("age");
+            System.out.println(age);
+
+            // getting address 
+            // Obtendo a posição do address no arquivo Json
+            Map address = ((Map) jo.get("address"));
+
+            // iterating address Map 
+            // Repetindo o addres no Map
+            // Map:: É uma estrutura de dados que permite que você ligue
+            // a cada valor a uma chave. Assim podemos acessar o valor pela chave
+            // A idéia de ser uma interface é poder trocar de impletamentação se muito impacto, facilitando a manutenção.
+            Iterator<Map.Entry> itr1 = address.entrySet().iterator();
+            while (itr1.hasNext()) {
+                Map.Entry pair = itr1.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+
+            // getting phoneNumbers 
+            // Obtendo o phoneNumbers
+            JSONArray ja = (JSONArray) jo.get("phoneNumbers");
+
+            // iterating phoneNumbers 
+            // Repetindo o phoneNumbers no Map
+            Iterator itr2 = ja.iterator();
+
+            while (itr2.hasNext()) {
+                itr1 = ((Map) itr2.next()).entrySet().iterator();
+                while (itr1.hasNext()) {
+                    Map.Entry pair = itr1.next();
+                    System.out.println(pair.getKey() + " : " + pair.getValue());
+                }
+            }
+        } catch (Exception e) 
         {
-            
-            jsonObj = (JsonObject) parser.parse(new FileReader("C:\\Users\\guilhermecr\\OneDrive - CATENO GESTAO DE CONTAS DE PAGAMENTO S.A\\Documentos\\NetBeansProjects\\AppKoala\\src\\LeitorJson\\norris.json"));
-            
-//            System.out.println(jsonObj.get("value"));
-           // JsonValue value = (JSONValue) jsonObj.get("value");
-           // System.out.println(value);
-                    
-//            dados.setType((String) jsonObj.get("type"));
-//            dados.setJoke((String) jsonObj.get("joke"));
-//            dados.setId((String) jsonObj.get("id"));
-//            dados.setCategories((String) jsonObj.get("categories"));
-//            
-//            System.out.println("Ai está: " + "\n" + dados.toString());
-;
-            
-        }catch(Exception e) 
-        {
+            System.out.println("Erro no sezrvidor!" + e);
             e.printStackTrace();
         }
+
     }
-    
 }
